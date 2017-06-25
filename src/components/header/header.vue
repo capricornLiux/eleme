332cb3fafc6d53dabf85d33bda83e531306d2e65
刘鑫 <784532927@qq.com>
2017-06-26 01:10:20 +0800

    设置header的公告内容和背景模糊效果

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -31,12 +31,28 @@
       </div>
       <!--右侧内容结束-->
 
+      <!--商家支持内容-->
+      <div class="support-count" v-if="seller.supports">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
+      <!--商家支持内容结束-->
+
     </div>
     <!--内容结束-->
 
     <!--新闻公告-->
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
     <!--新闻公告结束-->
+
+    <!--头部背景-->
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+    <!--头部背景结束-->
 
   </div>
 </template>
@@ -63,10 +79,13 @@
   /*css这里不能设置路径简写, js部分是可以的*/
 
   .header
+    position: relative
     color: rgb(255, 255, 255)
-    background-color: #000
+    background-color: rgba(7, 17, 27, 0.5);
     .content-wrapper
       padding: 24px 12px 18px 24px
+
+      position: relative
 
       /*解决头像和右侧文字之间有间隔空隙的问题, 设置父元素的字体大小为0(因为有空白字符导致的空隙问题)*/
       font-size: 0
@@ -126,4 +145,64 @@
           .text
             font-size: 10px
             line-height: 12px
+
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 18px
+        height: 24px
+        line-height: 24px
+        padding: 0 8px
+        border-radius: 12px
+        background-color: skyblue
+        text-align: center
+        .count
+          display: inline-block
+          /*width: 10px*/
+          font-size: 10px
+        .icon-keyboard_arrow_right
+          margin-left: 2px
+          line-height: 24px
+          font-size: 10px
+
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      background-color: rgba(7, 17, 27, 0.2)
+      /*padding:8px 12px;*/
+      /*padding: 0 12px*/
+      padding: 0 22px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        margin-top: 8px
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      /*margin-right: 4px*/
+      .bulletin-text
+        vertical-align: top
+        font-size: 10px
+        line-height: 28px
+        margin-right: 4px
+        margin-left: 4px
+      .icon-keyboard_arrow_right
+        position: absolute
+        right: 12px
+        top: 8px
+        font-size: 10px
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      z-index: -1
+      width: 100%
+      height: 100%
+      filter: blur(5px)
 </style>
