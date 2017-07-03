f5dfddb487e79c17858cf15eeff691ee785c45cb
刘鑫 <784532927@qq.com>
2017-07-03 20:14:25 +0800

    商品左右联动效果

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,7 @@
   <div class="goods">
     <div class="menu-wrapper" ref="menuwrapper">
       <ul class="menu-list">
-        <li class="menu-item" v-for="item in goods">
+        <li class="menu-item" v-for="(item,index) in goods" :class="{'current':currentIndex === index}" @click="selectMenu(index, $event)">
           <span class="text">
             <span class="icon" v-show="item.type>0" :class="classMap[item.type]"></span>{{item.name}}
           </span>
@@ -11,7 +11,7 @@
     </div>
     <div class="foods-wrapper" ref="foodswrapper">
       <ul>
-        <li v-for="type in goods" class="foods-list">
+        <li v-for="type in goods" class="foods-list food-list-hook">
           <!--一类食品标题-->
           <h1 class="type-title">{{type.name}}</h1>
           <!--一类食品标题结束-->
@@ -68,7 +68,10 @@
 
     data() {
       return {
-        goods: []
+        goods: [],
+        // 商品列表高度数组
+        listHeight: [],
+        scrollY: 0
       };
     },
 
@@ -81,7 +84,10 @@
 
           // 初始化的时候, vue更新dom的时候是异步的, 只有在nextTick方法中才会执行
           this.$nextTick(() => {
+            // 让左右列表能够滚动
             this._initScroll();
+            // 计算右侧高度集合
+            this._calculateHeight();
           });
         }
       });
@@ -90,9 +96,56 @@
     methods: {
       // 调用滚动方法
       _initScroll() {
-        this.menuScroll = new BScroll(this.$refs.menuwrapper, {});
+        this.menuScroll = new BScroll(this.$refs.menuwrapper, {
+          // 是否派发点击事件
+          click: true
+        });
+        this.foodsScroll = new BScroll(this.$refs.foodswrapper, {
+          probeType: 3
+        });
+        // 获取滚动距离
+        this.foodsScroll.on('scroll', (pos) => {
+//          console.log('滚动了');
+          this.scrollY = Math.abs(Math.round(pos.y));
+          console.log(this.scrollY);
+        });
+      },
 
-        this.foodsScroll = new BScroll(this.$refs.foodswrapper, {});
+      // 计算右侧商品列表的高度
+      _calculateHeight() {
+        // 获取列表dom
+        let foodList = this.$refs.foodswrapper.getElementsByClassName('food-list-hook');
+
+        let height = 0;
+
+        this.listHeight.push(height);
+
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
+      },
+      // 点击左侧的菜单按钮
+      selectMenu(index, event) {
+        let foodList = this.$refs.foodswrapper.getElementsByClassName('food-list-hook');
+        let el = foodList[index];
+        this.foodsScroll.scrollToElement(el, 1000);
+      }
+    },
+    // 定义计算型属性
+    computed: {
+      currentIndex() {
+        // 遍历当前高度列表
+        console.log('计算左侧索引的方法调用了');
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if (this.scrollY >= height1 && this.scrollY < height2) {
+            console.log(1);
+            return i;
+          }
+        }
       }
     }
   };
@@ -149,6 +202,11 @@
               &.special
                 bg-image('special_3')
 
+          &.current
+            background-color :#fff
+            .text
+              .icon
+                font-weight: 700
     .foods-wrapper
       flex: 1
       .foods-list
